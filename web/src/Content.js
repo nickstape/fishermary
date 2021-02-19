5c5fec1b6ee395b61653cd013b8e94c64e1d0822
anike fisher <anike.fisher@gmail.com>
2021-02-19 22:18:32 +0100

    git commit -m "Text Layout"

---
+++ web/src/Content.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+
+function Content() {
+  return (
+    <div className="Content">
+      <header className="Content-header">
+        <h2>Hi, I'm Mary</h2>
+      </header>
+      <div className="Content-body">
+        <p className="Content-body-paragraph-first">An aspiring  Product Manager passionate  about technology, photography, art and how they shape our human experience.</p>
+        <p className="Content-body-paragraph-second">Today, product managers are the glue that keeps the tech and design teams working together to build solutions that work for the rest of an organization. I am capable of managing a cross-function team in an agile environment and also hone soft skills necessary to carry out a product lifecycle.</p>
+        <p className="Content-body-paragraph-last">Based in Lagos, Nigeria.</p>
+      </div>
+    </div>
+  );
+}
+
+export default Content;
---
+++ web/src/Footer.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function Footer() {
+  return (
+    <div className="Content">
+      <footer className="Content-header">
+        <ul>
+          <li><a href='/https://linkedin.com/in/fisheranike/'>Linkedin</a></li>
+          <li><a href='/https://github.com/nickstape/'>Github</a></li>
+          <li><a href='/https://medium.com/@anike.fisher'>Medium</a></li>
+          <li><a href=''>Vsco</a></li>
+        </ul>
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
