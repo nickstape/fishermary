4515f8dade4daaedf5a411554c3d35314a6b56c9
anike fisher <anike.fisher@gmail.com>
2021-04-12 15:56:17 +0100

    chore: site setup

--- web/src/CaseStudies.js
+++ web/src/CaseStudies.js
@@ -1,12 +1,12 @@
-import { Button } from 'react-bootstrap';
 import history from './history';
+import casebody from './case-body.png';
 import './App.css';
 
 function CaseStudies() {
   return (
           <div className="container CaseStudies-body">   
                          <div data-content-for="xl">
-                            
+                           <img src={casebody} alt='case-study-placeholder' />
                         </div>
                 </div>
   );
